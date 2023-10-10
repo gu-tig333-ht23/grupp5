8347ab70ad4405db9ba1bb5371698aafe6bbaa29
Rotkvi <104556392+Wuplx@users.noreply.github.com>
2023-10-10 11:56:31 +0200

    ändrade i UI:n tog bort customscrollview och använder singlechildscrollview istället.

--- good_morning/lib/ui/daily_history_ui.dart
+++ good_morning/lib/ui/daily_history_ui.dart
@@ -9,116 +9,105 @@ class DailyHistoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: const Text('Good Morning'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: CustomScrollView(
-          slivers: <Widget>[
-            SliverList(
-              delegate: SliverChildListDelegate([
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          spreadRadius: 2,
-                          blurRadius: 5,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.only(
-                            right: 16,
-                            left: 16,
-                          ),
-                          child: Text(
-                            //hårdkodat just nu
-                            'Rubrik',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 20),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Text(
-                            //'Föddes Penei Sewell (American football player)',
-                            historyitem[0].text,
-                            style: TextStyle(fontSize: 18),
-                          ),
-                        ),
-                      ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: const Text('Good Morning'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 250, 250, 250),
+                    borderRadius: BorderRadius.circular(3),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text(
+                      //'Föddes Penei Sewell (American football player)',
+                      historyitem[0].text,
+                      style: TextStyle(fontSize: 18),
                     ),
                   ),
                 ),
-              ]),
-            ),
-            SliverPadding(
-              padding: const EdgeInsets.only(right: 15.0, left: 15.0),
-              sliver: SliverList(
-                delegate: SliverChildListDelegate([
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.3),
-                          spreadRadius: 2,
-                          blurRadius: 5,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.only(right: 8, left: 8, top: 8),
-                          child: Text('Name',
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 18)),
-                        ),
-                        Container(
-                          height: 500,
-                          width: 400,
-                          decoration: BoxDecoration(
-                            color: const Color(0xff7c94b6),
-                            image: DecorationImage(
-                              image: NetworkImage(
-                                historyitem[0].thumbnail,
-                                //'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Penei_Sewell_%2852480402764%29_%28cropped%29.jpg/320px-Penei_Sewell_%2852480402764%29_%28cropped%29.jpg'
-                              ),
-                              fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(3),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(right: 8, left: 8, top: 8),
+                        child: Text('Name',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18)),
+                      ),
+                      Container(
+                        height: 500,
+                        width: 400,
+                        decoration: BoxDecoration(
+                          color: const Color(0xff7c94b6),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              historyitem[0].thumbnail,
+                              //'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Penei_Sewell_%2852480402764%29_%28cropped%29.jpg/320px-Penei_Sewell_%2852480402764%29_%28cropped%29.jpg'
                             ),
+                            fit: BoxFit.cover,
                           ),
                         ),
-                        Padding(
-                            padding:
-                                EdgeInsets.only(right: 8, left: 8, bottom: 8),
-                            child: Text(
-                                //'Desctiption here: Penei Sewell is an American football offensive tackle for the Detroit Lions of the National Football League (NFL). He played college football at Oregon, where he won the Outland and Morris trophies in 2019.',
-                                historyitem[0].extract,
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16))),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ]),
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(3),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                      padding: EdgeInsets.only(right: 8, left: 8, bottom: 8),
+                      child: Text(
+                          //'Desctiption here: Penei Sewell is an American football offensive tackle for the Detroit Lions of the National Football League (NFL). He played college football at Oregon, where he won the Outland and Morris trophies in 2019.',
+                          historyitem[0].extract,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 16))),
+                ),
+              ),
+            ],
+          ),
+        ));
   }
 }
--- good_morning/lib/utils/daily_history.dart
+++ good_morning/lib/utils/daily_history.dart
@@ -30,7 +30,7 @@ List<HistoryItem> get historyitem => _historyitem;
 List<HistoryItem> _historyitem = [
   HistoryItem(
       'På den här dagen  för 23 år sedan föddes Penei Sewell (American football player)',
-      'massa information här nere, bla bla bla, föddes här, gick i skola där, 350 000 mål förra året.',
+      'massa information här nere, bla bla bla, föddes här, gick i skola där, 350 000 mål förra året. WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW',
       'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Penei_Sewell_%2852480402764%29_%28cropped%29.jpg/320px-Penei_Sewell_%2852480402764%29_%28cropped%29.jpg'),
 ];
 
