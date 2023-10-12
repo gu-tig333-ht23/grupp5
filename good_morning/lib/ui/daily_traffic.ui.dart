66b2e1d36da43c1181c4ccf7d70190ea5cf87183
Serena87 <gusstinst@student.gu.se>
2023-10-12 19:00:05 +0200

    layoutfix, card for hela trafficpage

--- good_morning/lib/ui/daily_traffic.ui.dart
+++ good_morning/lib/ui/daily_traffic.ui.dart
@@ -13,11 +13,11 @@ class DailyTrafficPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Card(
-              color: Theme.of(context).cardColor,
-              child: Container(
+        child: Card(
+          color: Theme.of(context).cardColor,
+          child: Column(
+            children: [
+              Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
                 ),
@@ -39,80 +39,82 @@ class DailyTrafficPage extends StatelessWidget {
                         ),
                       ),
                     ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 15, left: 10, right: 20),
+                      child: Row(
+                        children: [
+                          const Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    left: 8.0, top: 8, bottom: 8),
+                                child: Text('From:',
+                                    style: TextStyle(fontSize: 20)),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    left: 30, top: 8, right: 8.0, bottom: 8),
+                                child:
+                                    Text('To:', style: TextStyle(fontSize: 20)),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                              width:
+                                  20), // Adjust the spacing between the columns if needed
+                          Column(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(6.0),
+                                  child: buildSmallButton(
+                                    context,
+                                    'Parallellvägen 13E, Partille',
+                                    () {},
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 4.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(6.0),
+                                    child: buildSmallButton(
+                                      context,
+                                      'Medicinaregatan 15A, Göteborg',
+                                      () {},
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
-            ),
-            Row(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 15, left: 32, right: 20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(left: 8.0, top: 8, bottom: 8),
-                        child: Text('From:', style: TextStyle(fontSize: 20)),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            left: 30, top: 8, right: 8.0, bottom: 8),
-                        child: Text('To:', style: TextStyle(fontSize: 20)),
-                      ),
-                    ],
-                  ),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Image(
+                      image: AssetImage('lib/ui/images/exempelbildkarta.jpg'),
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20, bottom: 2),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: buildSmallButton(
-                            context,
-                            'Parallellvägen 13E, Partille',
-                            () {},
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 4.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(6.0),
-                            child: buildSmallButton(
-                              context,
-                              'Medicinaregatan 15A, Göteborg',
-                              () {},
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            Container(
-                child: const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Image(
-                      image: AssetImage('lib/ui/images/exempelbildkarta.jpg')),
-                ],
               ),
-            ))
-          ],
+            ],
+          ),
         ),
       ),
     );
