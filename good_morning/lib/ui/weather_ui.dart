e3e4ba34780ccc97862c76907c783d13a9a44e9e
IamNorthman <norrman20@gmail.com>
2023-10-18 23:47:26 +0200

    Connecting API

--- good_morning/lib/ui/weather_ui.dart
+++ good_morning/lib/ui/weather_ui.dart
@@ -1,4 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+String weatherAPIDayForecast =
+    'https://api.open-meteo.com/v1/forecast?latitude=57.7072&longitude=11.9668&hourly=temperature_2m&timezone=Europe%2FBerlin&forecast_days=1';
+
+String weatherAPICurrent =
+    'https://api.open-meteo.com/v1/forecast?latitude=57.7072&longitude=11.9668&current=temperature_2m&timezone=Europe%2FBerlin&forecast_days=1';
+
+Future<double> fetchCurretTemperature() async {
+  try {
+    final response = await http.get(Uri.parse(weatherAPICurrent));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonData = json.decode(response.body);
+      double currentTemperature = jsonData['current']['temperature_2m'];
+      return currentTemperature;
+    } else {
+      throw Exception('Failed to load data');
+    }
+  } catch (error) {
+    throw Exception('Error: $error');
+  }
+}
+
+Future<List<double>> fetchDailyForecast() async {
+  try {
+    final response = await http.get(Uri.parse(weatherAPIDayForecast));
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonData = json.decode(response.body);
+      List<dynamic> temperatureData = jsonData['hourly']['temperature_2m'];
+      List<double> temperaturesDay = temperatureData.cast<double>();
+      return temperaturesDay; // Return the list of temperature values
+    } else {
+      throw Exception('Failed to load data');
+    }
+  } catch (error) {
+    throw Exception('Error: $error');
+  }
+}
 
 class WeatherPage extends StatelessWidget {
   @override
@@ -10,158 +51,80 @@ class WeatherPage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.primary,
         title: const Text('Weather'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Container(
-            child: Column(
-              children: [
-                Icon(
-                  Icons.sunny,
-                  size: 75,
-                ),
-                Text(
-                  '10°C',
-                  textScaleFactor: 4,
-                ),
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Text('Torsdag   ', textScaleFactor: 2),
-                    Text('10°C'),
-                  ],
-                ),
-                Icon(
-                  Icons.sunny,
-                ),
-                Icon(Icons.arrow_forward)
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Text('Fredag    ', textScaleFactor: 2),
-                    Text('7°C'),
-                  ],
-                ),
-                Icon(Icons.water_drop),
-                Icon(Icons.arrow_forward)
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Text('Lördag    ', textScaleFactor: 2),
-                    Text('7°C'),
-                  ],
-                ),
-                Icon(Icons.water_drop),
-                Icon(Icons.arrow_forward)
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Text('Söndag    ', textScaleFactor: 2),
-                    Text('7°C'),
-                  ],
-                ),
-                Icon(Icons.water_drop),
-                Icon(Icons.arrow_forward)
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Text('Måndag    ', textScaleFactor: 2),
-                    Text('7°C'),
-                  ],
-                ),
-                Icon(Icons.water_drop),
-                Icon(Icons.arrow_forward)
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Text('Tisdag    ', textScaleFactor: 2),
-                    Text('7°C'),
-                  ],
-                ),
-                Icon(Icons.water_drop),
-                Icon(Icons.arrow_forward)
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    Text('Onsdag    ', textScaleFactor: 2),
-                    Text('7°C'),
-                  ],
-                ),
-                Icon(Icons.water_drop),
-                Icon(Icons.arrow_forward)
-              ],
-            ),
-          ),
-          IconButton(
-              iconSize: 50,
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: Text('Change location'),
-                        content: TextField(
-                          controller: weatherLocationController,
-                          decoration:
-                              InputDecoration(hintText: 'Type your location'),
-                        ),
-                        actions: <Widget>[
-                          TextButton(
-                            child: Text('Change'),
-                            onPressed: () {
-                              Navigator.of(context).pop();
-                            },
-                          ),
-                        ],
-                      );
-                    });
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FutureBuilder<double>(
+              future: fetchCurretTemperature(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  double currentTemp = snapshot.data ?? 0.0;
+                  return Text(
+                    '$currentTemp °C',
+                    style: TextStyle(fontSize: 40),
+                  );
+                }
               },
-              icon: Icon(Icons.place)),
-        ],
+            ),
+            FutureBuilder<List<double>>(
+              future: fetchDailyForecast(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  List<double> temperatures = snapshot.data!;
+
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    physics: ScrollPhysics(),
+                    itemCount: temperatures.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text('${index}: ${temperatures[index]} °C'),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+
+
+/* IconButton(
+          iconSize: 50,
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Change location'),
+                    content: TextField(
+                      controller: weatherLocationController,
+                      decoration:
+                          InputDecoration(hintText: 'Type your location'),
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                        child: Text('Change'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                });
+          },
+          icon: Icon(Icons.place)),*/
