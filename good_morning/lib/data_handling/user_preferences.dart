e4fdc1768d6e3328c90415bfc2ceb31d46b05c57
AugKra <131650115+AugKra@users.noreply.github.com>
2023-10-19 11:57:14 +0200

    Update storage

--- good_morning/lib/data_handling/user_preferences.dart
+++ good_morning/lib/data_handling/user_preferences.dart
@@ -1,6 +1,6 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-Future<void> saveUserSelectedCards(List<String> selectedCardKeys) async {
+Future<void> saveUserSelectedCards(List<String> selectedCardKeys ) async {
   // Implement code to save selected card keys to SharedPreferences
   final prefs = await SharedPreferences.getInstance();
   await prefs.setStringList('selectedCards', selectedCardKeys);
@@ -10,4 +10,24 @@ Future<List<String>> getUserSelectedCards() async {
   // Implement code to retrieve selected card keys from SharedPreferences
   final prefs = await SharedPreferences.getInstance();
   return prefs.getStringList('selectedCards') ?? [];
+}
+
+Future<void> setOnboardingCompleted(bool completed) async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setBool('onboardingCompleted', completed);
+}
+
+Future<bool> isOnboardingCompleted() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getBool('onboardingCompleted') ?? false;
+}
+
+Future<void> setUserName(String name) async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.setString('userName', name);
+}
+
+Future<String> getUserName() async {
+  final prefs = await SharedPreferences.getInstance();
+  return prefs.getString('userName') ?? '';
 }
