47aadd5806e9374a1538f03d802f855751277943
Rotkvi <104556392+Wuplx@users.noreply.github.com>
2023-10-29 18:23:41 +0100

    mer småfix
    tagit bort utkommenterad kod.
    
    lag till så att om år är null så blir det N/A

--- good_morning/lib/utils/daily_history/daily_history_api.dart
+++ good_morning/lib/utils/daily_history/daily_history_api.dart
@@ -4,18 +4,19 @@ import 'package:good_morning/utils/daily_history/daily_history_model.dart';
 import 'package:flutter/foundation.dart' show kDebugMode, kIsWeb;
 import 'dart:math';
 
-/*
-// API function1
-  Future<HistoryItem> fetchHistoryItemWiki(historyFilter, month, day) async {
+Future<HistoryItem> fetchHistoryItemWiki(historyFilter, month, day) async {
   final apiHeaderWiki = {
     'ContentType': 'application/json',
     'accept': 'application/json',
   };
+
   String wikiUrl;
   if (kIsWeb) {
+    // for running in web browsers, sends request through proxy server https://cors-anywhere.herokuapp.com (click there for access first!)
     wikiUrl =
         'https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/api/rest_v1/feed/onthisday';
   } else {
+    // for running in emulators, without proxy
     wikiUrl = 'https://en.wikipedia.org/api/rest_v1/feed/onthisday';
   }
 
@@ -23,114 +24,63 @@ import 'dart:math';
     Uri.parse('$wikiUrl/$historyFilter/$month/$day'),
     headers: apiHeaderWiki,
   );
-// mer arbete här
-  try {
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.body);
-      final events = data['$historyFilter'] as List;
 
-      var nextRandomNumber = Random().nextInt(events.length);
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> data = json.decode(response.body);
+    final events = data['$historyFilter'] as List;
+    if (events.isEmpty) {
+      if (kDebugMode) {
+        print('No items');
+      }
+      return HistoryItem(
+        text: '',
+        thumbnail: '',
+        extract: '',
+        year: '',
+      );
+    }
 
+    var nextRandomNumber = Random().nextInt(events.length);
+    Map<String, dynamic> item;
+    String text;
+    String thumbnail;
+    String extract;
+    String year;
+
+    do {
       if (nextRandomNumber >= events.length) {
-        nextRandomNumber = 0; // Wrap around to the beginning of the list
+        nextRandomNumber = Random().nextInt(events.length);
       }
-      var item = events[nextRandomNumber] as Map<String, dynamic>;
-      return HistoryItem.fromJson(item);
-    }
-  } catch (e) {
-    if (kDebugMode) {
-      print('An error occurred: $e');
-    }
-    throw Exception('Failed to load from API');
-  }
-
-  // Add a default return statement here, for example:
-  return HistoryItem(
-    text: 'no text',
-    thumbnail: 'no thumbnail',
-    extract: 'no extract',
-  ); // You should adjust this based on your actual data structure.
-}
-*/
-
-//API-function2
-
-  Future<HistoryItem> fetchHistoryItemWiki(
-      historyFilter, month, day) async {
-    final apiHeaderWiki = {
-      'ContentType': 'application/json',
-      'accept': 'application/json',
-    };
-
-    String wikiUrl;
-    if (kIsWeb) {
-      // for running in web browsers, sends request through proxy server https://cors-anywhere.herokuapp.com (click there for access first!)
-      wikiUrl =
-          'https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/api/rest_v1/feed/onthisday';
-    } else {
-      // for running in emulators, without proxy
-      wikiUrl = 'https://en.wikipedia.org/api/rest_v1/feed/onthisday';
-    }
-
-//for WEB: https://cors-anywhere.herokuapp.com/
-    final response = await http.get(
-      Uri.parse('$wikiUrl/$historyFilter/$month/$day'),
-      headers: apiHeaderWiki,
-    );
-
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.body);
-      final events = data['$historyFilter'] as List;
-      if (events.isEmpty) {
-        if (kDebugMode) {
-          print('No items');
-        }
-        return HistoryItem(
-          text:'',
-          thumbnail: '',
-          extract: '',
-          year: '',
-        );
+      item = events[nextRandomNumber] as Map<String, dynamic>;
+      text = item['text'] as String;
+      year = item['year'].toString();
+      if (item['year'] == null) {
+        year = 'N/A';
       }
-
-      var nextRandomNumber = Random().nextInt(events.length);
-      Map<String, dynamic> item;
-      String text;
-      String thumbnail;
-      String extract;
-      String year;
-
-      do {
-        if (nextRandomNumber >= events.length) {
-          nextRandomNumber = 0; // Wrap around to the beginning of the list
-        }
-        item = events[nextRandomNumber] as Map<String, dynamic>;
-        text = item['text'] as String;
-        year = item['year'].toString();
-        final pages = item['pages'] as List;
-        if (pages.isNotEmpty) {
-          final thumbnailData = pages[0]['thumbnail'];
-          if (thumbnailData != null && thumbnailData['source'] != null) {
-            thumbnail = thumbnailData['source'] as String;
-          } else {
-            thumbnail = '';
-          }
-          extract = pages[0]['extract'] as String;
+      final pages = item['pages'] as List;
+      if (pages.isNotEmpty) {
+        final thumbnailData = pages[0]['thumbnail'];
+        if (thumbnailData != null && thumbnailData['source'] != null) {
+          thumbnail = thumbnailData['source'] as String;
         } else {
           thumbnail = '';
-          extract = '';
         }
+        extract = pages[0]['extract'] as String;
+      } else {
+        thumbnail = '';
+        extract = '';
+      }
 
-        nextRandomNumber++;
-      } while (thumbnail.isEmpty);
+      nextRandomNumber++;
+    } while (thumbnail.isEmpty);
 
-      return HistoryItem(
-        text: text,
-        thumbnail: thumbnail,
-        extract: extract,
-        year: year,
-      );
-    } else {
-      throw Exception('Failed to load data from the API');
-    }
+    return HistoryItem(
+      text: text,
+      thumbnail: thumbnail,
+      extract: extract,
+      year: year,
+    );
+  } else {
+    throw Exception('Failed to load data from the API');
   }
+}
